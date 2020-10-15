4e3d70e698fb9e117c86c91eb689ca9d6e33d95a
Yahya Faroun <yahyafaroun@gmail.com>
2020-10-15 15:19:21 +0200

    user Infos

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
 const companyRoutes = require('./routes/companyRoutes')
+const userRoutes = require('./routes/userRoutes')
 const profileRoutes = require('./routes/profileRoutes')
-//const QRCode = require('qrcode')
 
-/*  Yahya   */
+
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('connected to db');
@@ -32,24 +32,10 @@ app.set('view engine', 'ejs')
 app.use('/auth', authRoutes)
 app.use('/profile', profileRoutes)
 app.use('/company', companyRoutes)
-
+app.use('/user', userRoutes)
 app.get('/', (req, res) => {
     res.render('index')
 })
-
-/*Yahya*/
-app.get('/user/companyID/tableID', (req, res) => {
-    res.render('userAnmeldung')
-})
-
-app.get('/user/companyID/tableID/userInfo', (req, res) => {
-    res.render('userInfo')
-})
-
-app.get('/user/companyID/tableID/companyInfo', (req, res) => {
-    res.render('companyInfo')
-})
-
 
 app.get('/')
 // app.get('/qrcode', async (req, res) => {
@@ -65,3 +51,6 @@ app.get('/')
 app.listen(process.env.PORT || 3000, () => {
     console.log('server listening at 3000')
 })
+
+
+///user/companyID/tableID/companyInfo redirect from userInfo to companyInfo
